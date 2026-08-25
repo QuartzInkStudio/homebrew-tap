b6cabb2a4fc7c41d96c94b3dae33ed7c4ea51acc
charles <charles@MacBook-Pro-3.local>
2026-08-25 14:50:52 +0800

    chore: update Facet to 1.0.19

--- Casks/facet.rb
+++ Casks/facet.rb
@@ -1,6 +1,6 @@
 cask "facet" do
-  version "1.0.18"
-  sha256 "4547f304dd0957c77c467eae60fa7a878877eefb2cf0fdcdffa3769462c25cf2"
+  version "1.0.19"
+  sha256 "cf04e7142ce07a054a9d3fee8dcf5d51860b422f24692a56f1104e330dbc0c91"
 
   url "https://github.com/QuartzInkStudio/Facet/releases/download/v#{version}/Facet-#{version}.dmg",
       verified: "github.com/QuartzInkStudio/Facet/"
